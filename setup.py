2432c7702f06614a46afc68181f5690a1c046289
Andrey Kislyuk <kislyuk@gmail.com>
2022-04-04 08:53:21 -0700

    Begin http-signature

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='http-signature',
+    version='0.0.1',
+    url='https://github.com/pyauth/http-signature',
+    license='Apache Software License',
+    author='Andrey Kislyuk',
+    author_email='kislyuk@gmail.com',
+    description="An implementation of the IETF HTTP Message Signatures draft standard",
+    long_description=open('README.md').read(),
+    use_scm_version={
+        "write_to": "http_signature/version.py",
+    },
+    setup_requires=['setuptools_scm >= 3.4.3'],
+    install_requires=[
+        "requests >= 2.27.1",
+        "http-sfv >= 0.9.3",
+        "cryptography >= 36.0.2"
+    ],
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    platforms=['MacOS X', 'Posix'],
+    test_suite='test',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
